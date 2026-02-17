03b463b0568e6dd249356f079fcfeb2224c0497f
Edwin Tchouateu <edwintchouateu54@gmail.com>
2026-02-17 21:19:19 +0000

    MISE A JOUR DU SPA

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 import AppRouter from './app/AppRouter';
 import { AuthProvider } from './context/AuthContext';
 import './index.css';
 
+const Router = import.meta.env.VITE_ROUTER_MODE === 'hash' ? HashRouter : BrowserRouter;
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <Router>
       <AuthProvider>
         <AppRouter />
       </AuthProvider>
-    </BrowserRouter>
+    </Router>
   </React.StrictMode>
 );
